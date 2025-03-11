42baf5f76634a538aa5ea2ea60039d515e8fdd02
charleycorneil <146792079+charleycorneil@users.noreply.github.com>
2025-03-11 17:12:33 -0400

    second commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Assignment 4
+
+### Live Link
+
+https://github.com/charleycorneil/midterm-project
--- server/server.js
+++ server/server.js
@@ -11,15 +11,11 @@ const server = http.createServer((req, res) => {
     req.url === "/" ? "index.html" : req.url
   );
 
-  // Explicitly handle requests for about.html by returning a 500 error
+  // Redirect about.html to 404.html
   if (req.url === "/about.html") {
-    res.writeHead(500, { "Content-Type": "text/html" });
-    return res.end(
-      "<h1>500 - Internal Server Error</h1><p>The about page is unavailable.</p>"
-    );
+    filePath = path.join(__dirname, "../public", "404.html");
   }
 
-  // Read file and serve response
   fs.readFile(filePath, (err, content) => {
     if (err) {
       fs.readFile(
